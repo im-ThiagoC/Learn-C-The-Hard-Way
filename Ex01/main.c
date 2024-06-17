623ee0c777f51113901ef614bfb39d21fd39e191
Thiago Carvalho <tcarvalho@digicarebiomedical.com>
2024-06-17 14:43:07 -0300

    Ex01 completed

--- Ex01/main.c
+++ Ex01/main.c
@@ -7,5 +7,13 @@ int main(int argc, char* argv[]) {
 	//this is also a comment
 	printf("You are %d miles away.\n", distance);
 
+	/*Extra Credit*/
+	printf("Hello"  " World\n");
+	printf("In ASCII code %c\n", 84);
+	printf("The value is:%3d \n", 1);
+	printf("The value is:%3d \n", 12);
+	printf("The value is:%3d \n", 123);
+	printf("The value is:%3d \n", 1234);
+
 	return 0;
 }
