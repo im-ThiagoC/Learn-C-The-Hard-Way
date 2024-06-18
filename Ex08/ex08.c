79fc75599a1a65e8f8c5873fd1fca2f0ad9a2336
Thiago Carvalho <tcarvalho@digicarebiomedical.com>
2024-06-18 12:16:06 -0300

    ex08 fix

--- Ex08/ex08.c
+++ Ex08/ex08.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[]){
-    int i = 0;
+    int i;
+    int temp = argc - 1;
+    if (!temp) {
+        printf("You have no arguments...");
 
-    if(argc == 1){
-        printf("You only have one argument. You suck.\n");
-    } else if (argc == 3 || argc == 5){
-        printf("You have 3 or 5 arguments! The last is %s\n", argv[argc-1]);
+    }   else if (temp == 3 || temp == 5){
+            printf("You have 3 or 5 arguments! The last is %s\n", argv[temp]);
 
-    } else if (argc > 1 && argc < 6) {
+    } else if (temp >= 1 && temp < 6) {
         printf("Here's your arguments:\n"); 
 
-        for(i = 0; i < argc; i++){
-            printf("%s\n", argv[i]);
+        for(i = 1; i <= temp; i++){
+            printf("%d - %s\n", i, argv[i]);
         }
     } else {
         printf("You have too many arguments... You suck.\n");
