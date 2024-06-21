c5c79919bcaaffbf8d66bb792426ca88e49d7c1a
Thiago Carvalho <tcarvalho@digicarebiomedical.com>
2024-06-21 14:39:36 -0300

    Sextou, ex18 + quicksorting

--- Ex18/ex18.c
+++ Ex18/ex18.c
@@ -8,8 +8,18 @@ typedef int (*compare_cb) (int a, int b);
 
 //Foward Functions
 void die(const char *message);
-void testSorting(int *numbers, int count, compare_cb cmp);
+void testSorting(int *numbers, int count, char sortType, char sortOrder);
+int partition(int *numbers, int low, int high);
+void swap(int *a, int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+//Sorting Functions
 int *bubbleSorting(int *numbers, int count, compare_cb cmp);
+int *quickSortingStart(int *numbers, int count);
+void quickSort(int *numbers, int low, int high);
 
 //Orders
 int sortedOrder(int a, int b);
@@ -17,14 +27,18 @@ int reversedOrder(int a, int b);
 int strangeOrder(int a, int b);
 
 int main (int argc, char* argv[]){
-    if(argc < 2){
-        die("USAGE: ex18 4 3 1 5 6");
+    if(argc < 4){
+        printf("Supported sortType:\n b = bubbleSort\n q = quickSort\n");
+        printf("Supported sortOrder:\n s = sorted\n r = reversed\nt=strange\n");
+        die("USAGE: ex18 <sortType> <sortOrder> 4 3 1 5 6");
     }
 
-    int count = argc - 1;
+    int count = argc - 3;
     int i = 0;
+    char sortType = argv[1][0];
+    char sortOrder = argv[2][0];
 
-    char **inputs = argv + 1;
+    char **inputs = argv + 3;
 
     int *numbers = malloc(count * sizeof(int));
     if(!numbers){
@@ -34,14 +48,69 @@ int main (int argc, char* argv[]){
     for(i = 0; i < count; i++){
         numbers[i] = atoi(inputs[i]);
     }
-
-    testSorting(numbers, count, sortedOrder);
-    testSorting(numbers, count, reversedOrder);
-    testSorting(numbers, count, strangeOrder);
+    
+    testSorting(numbers, count, sortType, sortOrder);
 
     free(numbers);
 
     return 0;
+}
+
+
+void testSorting(int *numbers, int count, char sortType, char sortOrder){
+    int i = 0;
+    int *sorted;
+    compare_cb cmp = sortedOrder;
+
+    //Switch for each Order, default is sorted
+    switch (sortOrder)
+    {
+    case 's':
+        cmp = sortedOrder;
+        break;
+    
+    case 'r':
+        cmp = reversedOrder;
+        break;
+
+    case 't':
+        cmp = strangeOrder;
+        break;
+    
+    default:
+        cmp = sortedOrder;
+        break;
+    }
+
+    //Switch for each Sort Algoritm, default is Bubble Sorting
+    switch (sortType)
+    {
+    case 'c':
+        printf("Bubble Sorting\n");
+        sorted = bubbleSorting(numbers, count, cmp);
+        break;
+    
+    case 'q':
+        printf("Quick Sorting, only Sorted Order\n");
+        sorted = quickSortingStart(numbers, count);
+        break;
+    
+    default:
+        printf("Default: Bubble Sorting\n");
+        sorted = bubbleSorting(numbers, count, cmp);
+        break;
+    }
+
+    if(!sorted){
+        die("Failed to sort as requested.");
+    }
+
+    for(i = 0; i < count; i++){
+        printf("%d", sorted[i]);
+    }
+    printf("\n");
+
+    free(sorted);
 }
 
 int *bubbleSorting(int *numbers, int count, compare_cb cmp){
@@ -69,20 +138,42 @@ int *bubbleSorting(int *numbers, int count, compare_cb cmp){
     return target;
 }
 
-void testSorting(int *numbers, int count, compare_cb cmp){
-    int i = 0;
-    int *sorted = bubbleSorting(numbers, count, cmp);
-
-    if(!sorted){
-        die("Failed to sort as requested.");
+int *quickSortingStart(int *numbers, int count){
+    int *target = malloc(count * sizeof(int));
+    if(!target){
+        die("Memory error on Target - QuickSortingStart");
     }
 
-    for(i = 0; i < count; i++){
-        printf("%d", sorted[i]);
-    }
-    printf("\n");
+    memcpy(target, numbers, count*sizeof(int));
 
-    free(sorted);
+    quickSort(target, 0, count-1);
+
+    return target;
+}
+
+void quickSort(int *target, int low, int high){
+    if(low < high){
+        int pivot = partition(target, low, high);
+
+        quickSort(target, low, pivot-1);
+        quickSort(target, pivot + 1, high);
+    }
+}
+
+int partition(int *target, int low, int high){
+    int pivot = target[high];
+    int i = low - 1;
+
+    for(int j = low; j <= high - 1; j++){
+        //If the element is lower or equal than pivot
+        if(target[j] <= pivot){
+            i++;
+            swap(&target[i], &target[j]);
+        }
+    }
+
+    swap(&target[i+1], &target[high]);
+    return i + 1;
 }
 
 void die(const char *message){
