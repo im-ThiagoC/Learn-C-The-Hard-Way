9f48e4f4a624a3a9e1b8a6a13d40fd9e127b1a23
Thiago Carvalho <tcarvalho@digicarebiomedical.com>
2024-06-24 13:25:51 -0300

    ex21 done

---
+++ Ex20/ex20.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "dbg.h"
+
+//A typedef creates a fake type, in this case for a function pointer
+typedef int (*compare_cb) (int a, int b);
+
+//Foward Functions
+void die(const char *message);
+void testSorting(int *numbers, int count, char sortType, char sortOrder);
+int partition(int *numbers, int low, int high);
+void swap(int *a, int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+//Sorting Functions
+int *bubbleSorting(int *numbers, int count, compare_cb cmp);
+int *quickSortingStart(int *numbers, int count);
+void quickSort(int *numbers, int low, int high);
+
+//Orders
+int sortedOrder(int a, int b);
+int reversedOrder(int a, int b);
+int strangeOrder(int a, int b);
+
+int main (int argc, char* argv[]){
+    if(argc < 4){
+        printf("Supported sortType:\n b = bubbleSort\n q = quickSort\n");
+        printf("Supported sortOrder:\n s = sorted\n r = reversed\n t=strange\n");
+        die("USAGE: ex18 <sortType> <sortOrder> 4 3 1 5 6");
+    }
+
+    int count = argc - 3;
+    int i = 0;
+    char sortType = argv[1][0];
+    char sortOrder = argv[2][0];
+
+    char **inputs = argv + 3;
+    
+    /*int *numbers = malloc(99999999999999999999 * sizeof(int));
+    if(!numbers){
+        die("Memory Error in Numbers.");
+    }*/
+   int *numbers = malloc(count * sizeof(int));
+   check(numbers, "Memory Error in Numbers");
+
+    for(i = 0; i < count; i++){
+        numbers[i] = atoi(inputs[i]);
+    }
+    
+    testSorting(numbers, count, sortType, sortOrder);
+
+    free(numbers);
+
+    return 0;
+
+    error:
+
+    return 1;
+}
+
+
+void testSorting(int *numbers, int count, char sortType, char sortOrder){
+    int i = 0;
+    int *sorted;
+    compare_cb cmp = sortedOrder;
+
+    //Switch for each Order, default is sorted
+    switch (sortOrder)
+    {
+    case 's':
+        cmp = sortedOrder;
+        break;
+    
+    case 'r':
+        cmp = reversedOrder;
+        break;
+
+    case 't':
+        cmp = strangeOrder;
+        break;
+    
+    default:
+        cmp = sortedOrder;
+        break;
+    }
+
+    //Switch for each Sort Algoritm, default is Bubble Sorting
+    switch (sortType)
+    {
+    case 'b':
+        printf("Bubble Sorting\n");
+        sorted = bubbleSorting(numbers, count, cmp);
+        break;
+    
+    case 'q':
+        printf("Quick Sorting, only Sorted Order\n");
+        sorted = quickSortingStart(numbers, count);
+        break;
+    
+    default:
+        printf("Default: Bubble Sorting\n");
+        sorted = bubbleSorting(numbers, count, cmp);
+        break;
+    }
+
+    if(!sorted){
+        die("Failed to sort as requested.");
+    }
+
+    for(i = 0; i < count; i++){
+        printf("%d ", sorted[i]);
+    }
+    printf("\n");
+
+    free(sorted);
+}
+
+int *bubbleSorting(int *numbers, int count, compare_cb cmp){
+    int temp = 0;
+    int i = 0;
+    int j = 0;
+    int *target = malloc(count * sizeof(int));
+
+    if(!target){
+        die("Memory error on target - bubbleSorting");
+    }
+
+    memcpy(target, numbers, count * sizeof(int));
+
+    for(i = 0; i < count; i++){
+        for(j = 0; j < count - 1; j++){
+            if(cmp(target[j], target[j+1]) > 0){
+                temp = target[j + 1];
+                target[j + 1] = target[j];
+                target[j] = temp;
+            }
+        }
+    }
+
+    return target;
+}
+
+int *quickSortingStart(int *numbers, int count){
+    int *target = malloc(count * sizeof(int));
+    if(!target){
+        die("Memory error on Target - QuickSortingStart");
+    }
+
+    memcpy(target, numbers, count*sizeof(int));
+
+    quickSort(target, 0, count-1);
+
+    return target;
+}
+
+void quickSort(int *target, int low, int high){
+    if(low < high){
+        int pivot = partition(target, low, high);
+
+        quickSort(target, low, pivot-1);
+        quickSort(target, pivot + 1, high);
+    }
+}
+
+int partition(int *target, int low, int high){
+    int pivot = target[high];
+    int i = low - 1;
+
+    for(int j = low; j <= high - 1; j++){
+        //If the element is lower or equal than pivot
+        if(target[j] <= pivot){
+            i++;
+            swap(&target[i], &target[j]);
+        }
+    }
+
+    swap(&target[i+1], &target[high]);
+    return i + 1;
+}
+
+void die(const char *message){
+    if(errno){
+        perror(message);
+    } else {
+        printf("ERROR: %s\n", message);
+    }
+
+    exit(1);
+}
+
+int sortedOrder(int a, int b){
+    return a - b;
+}
+
+int reversedOrder(int a, int b){
+    return b - a;
+}
+
+int strangeOrder(int a, int b){
+    if(a == 0 || b == 0){
+        return 0;
+    } else {
+        return a % b;
+    }
+}
---
+++ Ex21/ex21.c
@@ -0,0 +1,81 @@
+//Advanced Data Types and Flow Control
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+
+int main (){
+    int integer = 1;
+    double doublefloat = 27.3;
+    float floating = 3.33;
+    char character = 'T';
+    register char *string = "Thiago";
+    //void something
+    const enum colors{
+        red, green, blue
+    };
+
+    printf("CHAR_BIT    :   %d\n", CHAR_BIT);
+    printf("CHAR_MAX    :   %d\n", CHAR_MAX);
+    printf("CHAR_MIN    :   %d\n", CHAR_MIN);
+    printf("INT_MAX     :   %d\n", INT_MAX);
+    printf("INT_MIN     :   %d\n", INT_MIN);
+    printf("LONG_MAX    :   %ld\n", (long) LONG_MAX);
+    printf("LONG_MIN    :   %ld\n", (long) LONG_MIN);
+    printf("SCHAR_MAX   :   %d\n", SCHAR_MAX);
+    printf("SCHAR_MIN   :   %d\n", SCHAR_MIN);
+    printf("SHRT_MAX    :   %d\n", SHRT_MAX);
+    printf("SHRT_MIN    :   %d\n", SHRT_MIN);
+    printf("UCHAR_MAX   :   %d\n", UCHAR_MAX);
+    printf("UINT_MAX    :   %u\n", (unsigned int) UINT_MAX);
+    printf("ULONG_MAX   :   %lu\n", (unsigned long) ULONG_MAX);
+    printf("USHRT_MAX   :   %d\n", (unsigned short) USHRT_MAX);
+    
+    //<float.h>
+    printf("Storage size for float : %zu \n", sizeof(float));
+    printf("FLT_MAX     :   %g\n", (float) FLT_MAX);
+    printf("FLT_MIN     :   %g\n", (float) FLT_MIN);
+    printf("-FLT_MAX    :   %g\n", (float) -FLT_MAX);
+    printf("-FLT_MIN    :   %g\n", (float) -FLT_MIN);
+    printf("DBL_MAX     :   %g\n", (double) DBL_MAX);
+    printf("DBL_MIN     :   %g\n", (double) DBL_MIN);
+    printf("-DBL_MAX     :  %g\n", (double) -DBL_MAX);
+    printf("Precision value: %d\n", FLT_DIG );
+
+    //<stdint.h>
+    printf("int8_t: %d\n", sizeof(int8_t));
+    printf("uint8_t: %d\n", sizeof(uint8_t));
+    printf("int16_t: %d\n", sizeof(int16_t));
+    printf("uint16_t: %d\n", sizeof(uint16_t));
+    printf("int32_t: %d\n", sizeof(int32_t));
+    printf("uint32_t: %d\n", sizeof(uint32_t));
+    printf("int64_t: %d\n", sizeof(int64_t));
+    printf("uint64_t: %d\n", sizeof(uint64_t));
+    //Extra Credits
+    printf("int_least16_t: %d\n", sizeof(int_least16_t));
+    printf("uint_least32_t: %d\n", sizeof(uint_least32_t));
+    printf("INT_LEAST8_MAX: %d\n", sizeof(INT_LEAST8_MAX));
+    printf("INT_LEAST8_MIN: %d\n", sizeof(INT_LEAST8_MIN));
+    printf("UINT_LEAST64_MAX: %d\n", sizeof(UINT_LEAST64_MAX));
+    printf("int_fast16_t: %d\n", sizeof(int_fast16_t));
+    printf("uint_fast16_t: %d\n", sizeof(uint_fast16_t));
+    printf("INT_FAST8_MAX: %d\n", sizeof(INT_FAST8_MAX));
+    printf("UINT_FAST64_MAX: %d\n", sizeof(UINT_FAST64_MAX));
+    printf("intptr_t: %d\n", sizeof(intptr_t));
+    printf("uintptr_t: %d\n", sizeof(uintptr_t));
+    printf("INTPTR_MAX: %d\n", sizeof(INTPTR_MAX));
+    printf("INTPTR_MIN: %d\n", sizeof(INTPTR_MIN));
+    printf("UINTPTR_MAX: %d\n", sizeof(UINTPTR_MAX));
+    printf("intmax_t: %d\n", sizeof(intmax_t));
+    printf("uintmax_t: %d\n", sizeof(uintmax_t));
+    printf("INTMAX_MAX: %d\n", sizeof(INTMAX_MAX));
+    printf("INTMAX_MIN: %d\n", sizeof(INTMAX_MIN));
+    printf("UINTMAX_MAX: %d\n", sizeof(UINTMAX_MAX));
+    printf("PTRDIFF_MIN: %d\n", sizeof(PTRDIFF_MIN));
+    printf("PTRDIFF_MAX: %d\n", sizeof(PTRDIFF_MAX));
+    printf("SIZE_MAX: %d\n", sizeof(INT_FAST8_MIN));
+
+
+    return 0;
+}
